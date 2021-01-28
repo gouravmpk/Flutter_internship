5f0992c652829556bae78bb676b0154059942047
gourav <gouravmpk@gmail.com>
2021-01-28 11:54:43 +0530

    Hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,8 @@
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Images Here'),
-          ),
-          body: Image(
-            image: NetworkImage(
-                'https://c4.wallpaperflare.com/wallpaper/493/566/474/apex-legends-respawn-entertainment-ea-games-loba-loba-andrade-hd-wallpaper-preview.jpg'),
-          ),
-        ),
+        home: Text('Hello Tuna'),
       ),
     );
